eac958dc10c11f0a8393c32f4cf76b010d25d9e9
febolivar <fe.bolivar@uniandes.edu.co>
2017-03-22 09:26:59 -0500

    Paso 2 Iteracion 1: Se crea el test para cuando la cadena es vacia(rojo)
    Paso 2 Iteracion 1: Se crea el test para cuando la cadena es vacia(rojo)

--- KataTDD/EstadisticasTest.py
+++ KataTDD/EstadisticasTest.py
@@ -1,16 +1,29 @@
 from unittest import TestCase
-
 from Estadisticas import Estadisticas
 
+
 class EstadisticasTest(TestCase):
+    #Paso 1 Devolver un arreglo con el numero de elementos
+
+    #Iteracion 1
     def test_calcular(self):
         self.assertEqual(Estadisticas().calcular("")[0],0, "String Vacio")
 
+    # Iteracion 2
     def test_calcular_un_numero(self):
         self.assertEqual(Estadisticas().calcular("25")[0],1, "Ciclo un Numero")
 
+    # Iteracion 3
     def test_calcular_dos_numeros(self):
         self.assertEqual(Estadisticas().calcular("25,10")[0],2, "Ciclo con dos numeros, devuelve numero de elementos")
 
+    # Iteracion 4
     def test_calcular_varios_numeros(self):
         self.assertEqual(Estadisticas().calcular("25,10,12")[0],3, "Ciclo con varios numeros, devuelve numero de elementos")
+
+
+    # Paso 2 Devolver un arreglo con el numero de elementos y el minimo
+
+    # Iteracion 1
+    def test_calcular_minimo(self):
+        self.assertEqual(Estadisticas().calcular("")[1], 0, "String Vacio y minimo")
