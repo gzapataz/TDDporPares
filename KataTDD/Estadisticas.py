380939982aac9136e4cddef1700dd5e6ed8c31bc
febolivar <fe.bolivar@uniandes.edu.co>
2017-03-22 08:59:28 -0500

    Paso 1 Iteracion 3: Se crea el test para cuando la cadena tiene 2 numeros (rojo)
    Paso 1 Iteracion 3: Se crea el test para cuando la cadena tiene 2
    numeros (rojo)

--- KataTDD/Estadisticas.py
+++ KataTDD/Estadisticas.py
@@ -3,5 +3,12 @@ class Estadisticas:
         cadenaResp = [0]
         cadenaResp[0] = 0
 
-        if cadena =="":
+        if cadena == "":
             return cadenaResp
+        elif "," not in cadena:
+            cadenaResp[0] = 1
+            return cadenaResp
+
+
+
+
--- KataTDD/EstadisticasTest.py
+++ KataTDD/EstadisticasTest.py
@@ -5,3 +5,9 @@ from Estadisticas import Estadisticas
 class EstadisticasTest(TestCase):
     def test_calcular(self):
         self.assertEqual(Estadisticas().calcular("")[0],0, "String Vacio")
+
+    def test_calcular_un_numero(self):
+        self.assertEqual(Estadisticas().calcular("25")[0],1, "Ciclo un Numero")
+
+    def test_calcular_dos_numeros(self):
+        self.assertEqual(Estadisticas().calcular("25,10")[0],2, "Ciclo con dos numeros, devuelve numero de elementos")
