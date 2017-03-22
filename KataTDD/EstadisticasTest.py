1d11def4b00b3904e3d14aaec7a749c8a3c1328c
gzapataz <gab.zapata@uniandes.edu.co>
2017-03-22 09:05:42 -0500

    Paso 1 Iteracion 3: Se crea el test para cuando la cadena tiene varios numero(rojo)

--- KataTDD/EstadisticasTest.py
+++ KataTDD/EstadisticasTest.py
@@ -12,3 +12,5 @@ class EstadisticasTest(TestCase):
     def test_calcular_dos_numeros(self):
         self.assertEqual(Estadisticas().calcular("25,10")[0],2, "Ciclo con dos numeros, devuelve numero de elementos")
 
+    def test_calcular_varios_numeros(self):
+        self.assertEqual(Estadisticas().calcular("25,10,12")[0],3, "Ciclo con varios numeros, devuelve numero de elementos")
